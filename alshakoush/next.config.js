c80429f9c3f812e1b848903e85b770e166374445
Mohammad <mo.alshakoush@gmail.com>
2022-06-30 13:07:07 +0200

    changing nextjs config

--- alshakoush/next.config.js
+++ alshakoush/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix : '/alshakoush/out/',
+//  assetPrefix : '/alshakoush/out/',
 }
 
 module.exports = nextConfig
